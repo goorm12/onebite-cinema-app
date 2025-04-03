454c14e226ebc62b608d2017d61cba8fb95a478e
binwoonki <vinwoonki98@gmail.com>
2025-04-03 14:43:25 +0900

    Day - 10 SearchBar View 구현

---
+++ app/(anon)/(with-searchbar)/search/components/searchBarView.tsx
@@ -0,0 +1,34 @@
+interface SearchBarViewProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+  searchValue: string;
+}
+
+const SearchBarView = ({
+  onSubmit,
+  setSearchValue,
+  searchValue,
+}: SearchBarViewProps) => {
+  return (
+    <form onSubmit={onSubmit} className="flex flex-row gap-x-2.5">
+      <input
+        type="text"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setSearchValue(e.target.value)
+        }
+        value={searchValue}
+        aria-label="검색 입력 창"
+        className="border-2 border-red-400 px-2 py-1 rounded "
+      />
+
+      <button
+        type="submit"
+        className="bg-red-400 px-2.5 py-2 rounded text-white"
+      >
+        검색
+      </button>
+    </form>
+  );
+};
+
+export default SearchBarView;
