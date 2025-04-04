89cec793f1dfa35a1262b7366d4f2191ec0a8204
binwoonki <vinwoonki98@gmail.com>
2025-04-04 17:01:05 +0900

    Day - 11 글로벌 레이아웃 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,9 +8,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
-        <div>Global Layout</div>
-        {children}
+      <body className="p-0 m-0 bg-black text-white">
+        <div className="container-box-[600px] pt-5 flex flex-col gap-5">
+          <Link href={"/"}>
+            <h1 className="text-(--color-soft-red) font-extrabold">
+              ONEBITE CINEMA
+            </h1>
+          </Link>
+          {children}
+        </div>
       </body>
     </html>
   );
