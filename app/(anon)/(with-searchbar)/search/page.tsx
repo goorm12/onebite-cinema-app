7ac3c3c27e41bb66de078cd65b84c95e92665149
binwoonki <vinwoonki98@gmail.com>
2025-04-04 17:01:56 +0900

    Day - 11 검색 결과 페이지 UI 작업

--- app/(anon)/(with-searchbar)/search/page.tsx
+++ app/(anon)/(with-searchbar)/search/page.tsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
+import { MovieData } from "@/app/types/types";
+import dummyData from "@/app/data/dummy.json";
+import MovieItem from "@/app/components/movieItem";
+
 const Page = async ({
   searchParams,
 }: {
   searchParams: Promise<{ q: string }>;
 }) => {
   const { q } = await searchParams;
-  return <div>Search : {q}</div>;
+
+  // const movies: MovieData[] = dummyData.filter((movie) =>
+  //   movie.title.includes(q)
+  // );
+
+  // if (!movies.length) return <div>영화를 찾을 수 없습니다.</div>;
+  return (
+    <section>
+      <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3  gap-3.5">
+        {dummyData.map((movie) => (
+          <MovieItem key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Page;
