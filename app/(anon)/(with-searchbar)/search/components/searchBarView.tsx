cd0ea75df55dbc29bf836b36b83567012236edc4
binwoonki <vinwoonki98@gmail.com>
2025-04-04 17:02:21 +0900

    Day - 11 SearchBarView style 추가

--- app/(anon)/(with-searchbar)/search/components/searchBarView.tsx
+++ app/(anon)/(with-searchbar)/search/components/searchBarView.tsx
@@ -10,7 +10,7 @@ const SearchBarView = ({
   searchValue,
 }: SearchBarViewProps) => {
   return (
-    <form onSubmit={onSubmit} className="flex flex-row gap-x-2.5">
+    <form onSubmit={onSubmit} className="flex flex-row gap-x-2.5 w-full">
       <input
         type="text"
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -18,12 +18,12 @@ const SearchBarView = ({
         }
         value={searchValue}
         aria-label="검색 입력 창"
-        className="border-2 border-red-400 px-2 py-1 rounded "
+        className="border-2 border-red-400 px-2 py-1 rounded flex-1"
       />
 
       <button
         type="submit"
-        className="bg-red-400 px-2.5 py-2 rounded text-white"
+        className="bg-red-400 px-2.5 py-2 rounded text-white "
       >
         검색
       </button>
