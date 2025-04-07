4ee78fa00dde44aaf6dc001a60b194dd1de01461
binwoonki <vinwoonki98@gmail.com>
2025-04-07 15:29:05 +0900

    Day 12 : 인덱스 페이지 추천 영화, 모든 영화 컴포넌트 분리 및 Suspense 추가 데이터 페칭 시 보여질 UI fallback 추가

--- app/(anon)/(with-searchbar)/page.tsx
+++ app/(anon)/(with-searchbar)/page.tsx
@@ -1,25 +1,15 @@
-import MovieItem from "@/app/components/movieItem";
-import dummyData from "@/app/data/dummy.json";
+import { Suspense } from "react";
+import AllMovies from "./components/allMovies";
+import RecoMovies from "./components/recoMovies";
 const Home = () => {
   return (
     <div className="flex flex-col gap-5">
-      <article className="flex flex-col gap-4">
-        <h1>지금 가장 추천하는 영화</h1>
-        <div className="grid grid-cols-3 gap-3.5">
-          {dummyData.slice(0, 3).map((movie) => (
-            <MovieItem key={movie.id} movie={movie} />
-          ))}
-        </div>
-      </article>
-
-      <article className="flex flex-col gap-4">
-        <h1>등록된 모든 영화</h1>
-        <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-3.5">
-          {dummyData.map((movie) => (
-            <MovieItem key={movie.id} movie={movie} />
-          ))}
-        </div>
-      </article>
+      <Suspense fallback={<div>🎬 추천 영화 로딩 중...</div>}>
+        <RecoMovies />
+      </Suspense>
+      <Suspense fallback={<div>📽 전체 영화 로딩 중...</div>}>
+        <AllMovies />
+      </Suspense>
     </div>
   );
 };
