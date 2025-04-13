3fc9aa627bc3852d9c0d8e22e2a35f3cafbf9df5
binwoonki <vinwoonki98@gmail.com>
2025-04-13 16:47:54 +0900

    Day 16 : 영화 상세 페이지 컴포넌트 분리

--- app/(anon)/movie/[id]/page.tsx
+++ app/(anon)/movie/[id]/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
 import { MovieData } from "@/app/types/types";
-import { notFound } from "next/navigation";
+import MovieDetail from "./components/movieDetail";
+import ReviewEditor from "./components/reviewEditor";
+import ReviewList from "./components/reviewList";
 
 export const generateStaticParams = async () => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movie`, {
@@ -16,58 +17,11 @@ export const generateStaticParams = async () => {
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/movie/${id}`,
-    {
-      cache: "force-cache",
-    }
-  );
-
-  if (!response.ok) {
-    if (response.status === 404) {
-      notFound();
-    }
-    <div>영화를 찾을 수 없습니다.</div>;
-  }
-
-  const movie: MovieData = await response.json();
-
-  if (!movie) return <div>영화를 찾을 수 없습니다.</div>;
-
   return (
-    <section className="flex flex-col gap-5 pt-5 pb-5">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          backgroundImage: `url(${movie?.posterImgUrl})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          position: "relative",
-        }}
-        className="before-overlay rounded-lg"
-      >
-        <div className="z-10 pt-5 pb-5 ">
-          <Image
-            src={movie?.posterImgUrl}
-            alt={movie?.title}
-            width={300}
-            height={500}
-            className="rounded-lg"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col gap-3">
-        <h1 className="text-2xl font-bold">{movie.title}</h1>
-        <div className="flex flex-row gap-2 text-sm">
-          <p>{movie.releaseDate} /</p>
-          <p>{movie.genres} /</p>
-          <p>{movie.runtime}분</p>
-        </div>
-        <p className="text-sm">{movie.company}</p>
-        <p>{movie.subTitle}</p>
-        <p>{movie.description}</p>
-      </div>
+    <section className="flex flex-col gap-5">
+      <MovieDetail id={id} />
+      <ReviewEditor movieId={id} />
+      <ReviewList movieId={id} />
     </section>
   );
 };
