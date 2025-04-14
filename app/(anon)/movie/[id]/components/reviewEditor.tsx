821e25823a2aa4cecb9299e81b3a9676447e6502
binwoonki <vinwoonki98@gmail.com>
2025-04-14 16:29:33 +0900

    Day 17 : useAction을 이용한 리뷰 작성

--- app/(anon)/movie/[id]/components/reviewEditor.tsx
+++ app/(anon)/movie/[id]/components/reviewEditor.tsx
@@ -1,15 +1,29 @@
+"use client";
+import { useActionState, useEffect } from "react";
 import { createReviewAction } from "../actions/createReviewAction.action";
 
 const ReviewEditor = ({ movieId }: { movieId: string }) => {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className="flex flex-col gap-4" action={createReviewAction}>
+      <form className="flex flex-col gap-4" action={formAction}>
         <input name="movieId" readOnly value={movieId} hidden />
         <textarea
           name="content"
           className="border-b-neutral-200 border-1 w-full p-2 rounded-sm"
           placeholder="내용을 입력해주세요"
           required
+          disabled={isPending}
         />
         <div className="flex gap-4 justify-end">
           <input
@@ -18,12 +32,13 @@ const ReviewEditor = ({ movieId }: { movieId: string }) => {
             placeholder="작성자"
             className="border-b-neutral-200 border-1 py-2 px-4 rounded-sm"
             required
+            disabled={isPending}
           />
           <button
             type="submit"
             className="bg-neutral-100 py-2 px-4 text-black rounded-sm cursor-pointer"
           >
-            작성하기
+            {isPending ? "저장중" : "작성하기"}
           </button>
         </div>
       </form>
